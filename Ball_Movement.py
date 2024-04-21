bb47b10375ecd271df9f3de82d5d8a785f2bfaea
AhmedMu7 <mo190035@gmail.com>
2024-04-21 19:39:22 +0200

    Ball_Movement

---
+++ Ball_Movement.py
@@ -0,0 +1,36 @@
+def reset_ball():
+    global ball_speed_x, ball_speed_y
+    ball.x = screen_width/2 - 10
+    ball.y = random.randint(10,100)
+    ball_speed_x *= random.choice([-1,1])
+    ball_speed_y *= random.choice([-1,1])
+
+
+def animate_ball():
+    global ball_speed_x, ball_speed_y
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.bottom >= screen_height or ball.top <= 0:
+        ball_speed_y *= -1
+
+    if ball.right >= screen_width:
+        point_won("cpu")
+
+    if ball.left <= 0:
+        point_won("player")
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+    if ball.colliderect(player) or ball.colliderect(cpu):
+        ball_speed_x *= -1
+
+        
+ball_speed_x = 6
+ball_speed_y = 6
+
+
+ball = pygame.Rect(0,0,30,30)
+ball.center = (screen_width/2, screen_height/2)
+
+
+
+
